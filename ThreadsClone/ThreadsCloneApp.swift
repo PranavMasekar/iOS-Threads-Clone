089351dbc9db07f5bcc0398322c955bde56f2c3a
PranavMasekar <pranavmasekar4@gmail.com>
2025-05-24 11:37:37 +0530

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Pranav Masekar on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
